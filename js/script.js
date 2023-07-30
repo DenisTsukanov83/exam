075fd6f0e852c946542aacc4188d0a9fd18f38c1
Denis Tsukanov <tsukanovdenis83@yandex.ru>
2023-07-30 16:52:43 +0400

    added donation and modals

--- js/script.js
+++ js/script.js
@@ -91,99 +91,125 @@ addEventListener("DOMContentLoaded", () => {
             "parasites": ["lice", "fleas"]
         }
     ];
-    
+
     const friendsContainer = document.querySelector('.friends-container');
     const btnsLeft = document.querySelectorAll('.friends-left');
     const btnsRight = document.querySelectorAll('.friends-right');
+    const modals = document.querySelector('.modals');
     let access = true;
-    
+
     let currentIndex = 0;
-    
-    
+
+
     function createItem(i) {
         const item = document.createElement('div');
-        const img = document.createElement('img');
-        const h5 = document.createElement('h5');
-        const btn =document.createElement('button');
-    
-        img.setAttribute('src', `${pets[i].img}`);
-        h5.textContent = `${pets[i].name}`;
-        btn.className = 'learn';
-        btn.textContent = 'Learn more';
         item.className = 'friends-item';
-    
-        item.append(img, h5, btn);
+        item.innerHTML = `
+                            <img src=${pets[i].img}>
+                            <h5>${pets[i].name}</h5>
+                            <a class="waves-effect waves-light modal-trigger learn" href="#modal${i + 1}">Learn more</a>
+                        `;
+        createModals(i, item);
+        $('.modal').modal(); //Инициализация модальных окон
         return item;
     }
-    
+
+    function createModals(i, item) {
+        const modal = document.createElement('div');
+        modal.setAttribute('id', `modal${i + 1}`);
+        modal.className = 'modal';
+        modal.innerHTML = `
+                            <div class="modal-content">
+                                <div class="modal-photo">
+                                    <img src=${pets[i].img}>
+                                </div>
+                                <div class="modal-param">
+                                    <h5>${pets[i].name}</h5>
+                                    <div class="modal-subtitle">${pets[i].type} - ${pets[i].breed}</div>
+                                    <div class="modal-descr">${pets[i].description}</div>
+                                    <ul class="modal-list">
+                                        <li>Age: <span>${pets[i].age}</span></li>
+                                        <li>Inoculations: <span>${pets[i].inoculations}</span></li>
+                                        <li>Diseases: <span>${pets[i].diseases}</span></li>
+                                        <li>Parasites: <span>${pets[i].parasites}</span></li>
+                                    </ul>
+                                    <div class="modal-footer">
+                                        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+                                    </div>
+                                </div>
+                            </div>
+                        `;
+        item.append(modal);
+    }
+
     friendsContainer.append(createItem(1), createItem(2), createItem(3), createItem(4), createItem(5));
-    
+
     function appendEl() {
         access = false;
         const item = createItem(currentIndex);
 
         friendsContainer.querySelectorAll('.friends-item').forEach(el => {
-            switch(true) {
+            switch (true) {
                 case window.innerWidth > 1000: el.classList.add('to-left');
-                break;
+                    break;
                 case window.innerWidth <= 1000 && window.innerWidth > 768: el.classList.add('to-left-l');
-                break;
+                    break;
                 case window.innerWidth <= 768: el.classList.add('to-left-s');
-                break;
+                    break;
             }
         })
 
-        setTimeout( () => {
+        setTimeout(() => {
             friendsContainer.append(item);
-            while(friendsContainer.querySelectorAll('.friends-item').length > 5) {
+            while (friendsContainer.querySelectorAll('.friends-item').length > 5) {
                 friendsContainer.firstChild.remove();
             }
 
             friendsContainer.querySelectorAll('.friends-item').forEach(el => {
                 el.classList.remove('to-left', 'to-left-l', 'to-left-s');
-                
+
             })
             access = true;
         }, 450)
     }
-    
+
     function prependEl() {
         access = false;
         const item = createItem(currentIndex);
 
         friendsContainer.querySelectorAll('.friends-item').forEach(el => {
-            switch(true) {
+            switch (true) {
                 case window.innerWidth > 1000: el.classList.add('to-right');
-                break;
+                    break;
                 case window.innerWidth <= 1000 && window.innerWidth > 768: el.classList.add('to-right-l');
-                break;
+                    break;
                 case window.innerWidth <= 768: el.classList.add('to-right-s');
-                break;
+                    break;
             }
         })
 
-        setTimeout( () => {
+        setTimeout(() => {
             friendsContainer.prepend(item);
-    
-            while(friendsContainer.querySelectorAll('.friends-item').length > 5) {
+
+            while (friendsContainer.querySelectorAll('.friends-item').length > 5) {
                 friendsContainer.lastChild.remove();
             }
 
             friendsContainer.querySelectorAll('.friends-item').forEach(el => {
                 el.classList.remove('to-right', 'to-right-l', 'to-right-s');
-                
+
             })
             access = true;
         }, 450)
 
-        
+
     }
-    
+
     function incr() {
         currentIndex < pets.length - 1 ? currentIndex++ : currentIndex = 0;
-        
+
     }
-    
+
     function decr() {
         currentIndex > 0 ? currentIndex-- : currentIndex = pets.length - 1;
     }
@@ -191,7 +217,7 @@ addEventListener("DOMContentLoaded", () => {
     function addEventList(btns, index, func) {
         btns.forEach(el => {
             el.addEventListener('click', () => {
-                if(access) {
+                if (access) {
                     index();
                     func();
                 }
